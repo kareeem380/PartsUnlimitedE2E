1be3d91286390416b03b44d6c014eac95dd59ddb
Karim <karimbouaddi380@gmail.com>
2023-03-30 11:50:56 +0000

    Added comments

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -5,7 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
     {
-        //First Commit
+        //Second Commit
         [Key]
         public int CartItemId { get; set; }
 
